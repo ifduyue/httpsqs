07e9f136f3240ca6bc01f04bf1d1e7338465c0e1
lb13810398408 <lb13810398408@f43af418-e918-11de-9298-fbd6a473cdd1>
2010-03-23 02:37:07 +0000

    optimize c client

--- client/c/httpsqs_client.c
+++ client/c/httpsqs_client.c
@@ -4,6 +4,7 @@
  *可以修改处理队列数据部分的代码实现其他的数据处理
  *本程序只是一个最初级的作品，本人的C语言水平也很低（在写这个程序前还不会C语言的socket发送HTTP请求）
  *希望张宴大哥给予指正与修改，体现我们开源的优势～哈哈
+ *
  *使用方法：
  *gcc -o httpsqs_client http_client.c
  *./httpsqs_client -q your_queue_name -t 20(每秒获取队列的次数)
@@ -27,7 +28,7 @@
 #define HttpsqsIp "192.168.1.102"
 #define HttpsqsPort 1218
 
-#define RemoteIp "xxx.xxx.xxx.xxx"//处理端IP
+#define RemoteIp "121.52.218.137"
 #define RemotePort 80
 
 void init_daemon(void);
@@ -96,21 +97,97 @@ char *getresult(char *data)
     return res;
 }
 
-int main(int argc,char **argv) 
-{ 
-    FILE * fp;
-    char queuename[20];
-    int loop, m;
-    int black_sock;
-	int post_sock;
+void process(char *queuename, int loop)
+{
+	FILE * fp;
+	int m;
+	int black_sock;
     char data[3];
     char * str;
-    char res[5000];
-	char res_2[5000];
     char pattern[] = "Connection: close";
 	char pattern_2[] = "Array";
 	int len = 0;
 	int i = 0;
+
+	for(m = 0; m < loop; m++)
+    {
+	    memset(str, 0, strlen(str));
+        len = 0;
+        i = 0;
+        memset(data, 0, strlen(data));
+
+        black_sock = htconnect(HttpsqsIp, HttpsqsPort);
+	    if (black_sock < 0) return;
+
+	    htsend(black_sock, "GET /?charset=utf-8&name=%s&opt=get HTTP/1.1\r\n", queuename, 10);
+	    htsend(black_sock, "Host: %s\r\n", HttpsqsIp, 10);
+	    htsend(black_sock, "Connection: close\r\n", 10);
+	    htsend(black_sock, "\r\n", 10);
+
+	    i = 0;
+	    while (read(black_sock, data, 1) > 0)
+    	{
+		    str[i] = data[0];
+		    i++;
+	    }
+	    close(black_sock);
+        memset(data, 0, strlen(data));
+
+        strcpy(str, strstr(str, pattern));
+        strcpy(str, getresult(str));
+
+	    if(strchr(str, 'H') == NULL)
+	    {
+		    if((fp = fopen("test.log", "a")) >= 0)
+			    fprintf(fp, "GET FROM HTTPSQS %s\r\n", str);
+
+		    black_sock = htconnect(RemoteIp, RemotePort);
+		    if (black_sock < 0) return;
+
+		    len = strlen(str) + 5;
+		    htsend(black_sock, "POST /index.php?m=data&a=update HTTP/1.1\r\n", 10);
+		    htsend(black_sock, "Content-type: application/x-www-form-urlencoded\r\n", 10);
+		    htsend(black_sock, "Host: www.oooffice.com\r\n", 10);
+		    htsend(black_sock, "Content-Length: %d\r\n", len, 10);
+		    htsend(black_sock, "Connection: close\r\n", 10);
+		    htsend(black_sock, "\r\n", 10);
+		    htsend(black_sock, "data=%s", str, 10);
+
+		    memset(str, 0, strlen(str));
+		    i = 0;
+		    while (read(black_sock, data, 1)>0)
+		    {
+                if(data[0] == '|')
+                {
+                    if(str[i-1] == 'D' && str[i-2] == 'N' && str[i-3] == 'E')
+                    {
+                        str[i-3] = '\0';
+                        break;
+                    }
+                }
+			    str[i] = data[0];
+			    i++;
+		    }
+            memset(data, 0, strlen(data));
+            close(black_sock);
+		    if(fp >= 0)
+		    {
+			    if(strlen(str))
+			    {
+				    strcpy(str, strstr(str, pattern_2));
+				    fprintf(fp, "POST TO REMOTE\r\n%s\r\n", str);
+			    }
+			    fclose(fp);
+		    }
+	    }
+    }
+	return;
+}
+
+int main(int argc,char **argv) 
+{ 
+    char queuename[20];
+    int loop;
 
     if(argc <= 4 || strcmp(argv[1], "-q") != 0 || strcmp(argv[3], "-t") != 0)
     {
@@ -133,83 +210,6 @@ int main(int argc,char **argv)
     while(1)
     {
         sleep(1);
-        for(m = 0; m < loop; m++)
-        {
-		    memset(str, 0, strlen(str));
-            memset(res, 0, strlen(res));
-            memset(res_2, 0, strlen(res_2));
-            len = 0;
-            i = 0;
-            memset(data, 0, strlen(data));
-
-            black_sock = htconnect(HttpsqsIp, HttpsqsPort);
-		    if (black_sock < 0) return;
-
-		    htsend(black_sock, "GET /?charset=utf-8&name=%s&opt=get HTTP/1.1\r\n", queuename, 10);
-		    htsend(black_sock, "Host: %s\r\n", HttpsqsIp, 10);
-		    htsend(black_sock, "Connection: close\r\n", 10);
-		    htsend(black_sock, "\r\n", 10);
-
-		    i = 0;
-		    while (read(black_sock, data, 1) > 0)
-	    	{
-			    str[i] = data[0];
-			    i++;
-		    }
-		    close(black_sock);
-            memset(data, 0, strlen(data));
-
-		    strcpy(res, strstr(str, pattern));
-		    memset(str, 0, strlen(str));
-		    strcpy(str, getresult(res));
-
-		    if(strchr(str, 'H') == NULL)
-		    {
-                //以下处理过程请自行制定
-			    if((fp = fopen("test.log", "a")) >= 0)
-				    fprintf(fp, "GET FROM HTTPSQS %s\r\n", str);
-
-			    post_sock = htconnect(RemoteIp, RemotePort);
-			    if (post_sock < 0) return;
-
-			    len = strlen(str) + 5;
-			    htsend(post_sock, "POST /index.php?m=data&a=update HTTP/1.1\r\n", 10);//修改处理端文件
-			    htsend(post_sock, "Content-type: application/x-www-form-urlencoded\r\n", 10);
-			    htsend(post_sock, "Host: www.CHANGETOYUREDOMAIN.com\r\n", 10);//您自己的域名
-			    htsend(post_sock, "Content-Length: %d\r\n", len, 10);
-			    htsend(post_sock, "Connection: close\r\n", 10);
-			    htsend(post_sock, "\r\n", 10);
-			    htsend(post_sock, "data=%s", str, 10);
-
-			    memset(str, 0, strlen(str));
-			    i = 0;
-			    while (read(post_sock, data, 1)>0)
-			    {
-                    if(data[0] == '|')
-                    {
-                        if(str[i-1] == 'D' && str[i-2] == 'N' && str[i-3] == 'E')
-                        {
-                            str[i-3] = '\0';
-                            break;
-                        }
-                    }
-				    str[i] = data[0];
-				    i++;
-			    }
-                memset(data, 0, strlen(data));
-                close(post_sock);
-			    if(fp >= 0)
-			    {
-				    if(strlen(str))
-				    {
-					    strcpy(res_2, strstr(str, pattern_2));
-					    memset(str, 0, strlen(str));
-					    strcpy(str, res_2);
-					    fprintf(fp, "POST TO REMOTE\r\n%s\r\n", str);
-				    }
-				    fclose(fp);
-			    }
-		    }
-        }
+        process(queuename, loop);
     }
 }
